6a35e55252f71054571c06a4b5d1f5dbf442154b
Apor0 <142001340+Apor0@users.noreply.github.com>
2023-08-22 20:58:13 +0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DropdownDirecrtive } from './shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeService } from './recipes/recipe.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    DropdownDirecrtive,
+    RecipeStartComponent,
+    RecipeEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [ShoppingListService, RecipeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model'
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
+
+@Injectable()
+export class RecipeService{
+  recipesChanged = new Subject<Recipe[]>();
+
+  private recipes: Recipe[]=[
+    new Recipe(
+      'Schnitzel',
+      'schnitzel with fries',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_KN_vaXat9kpJiil2tx-WN4yA0XKKbUz9IRjklTOvig&s',
+        [
+          new Ingredient('Meat', 1),
+          new Ingredient('Bag of fries', 1), 
+        ]),
+    new Recipe(
+      'Burger',
+      'a burger',
+      'https://media.istockphoto.com/id/1448322070/photo/fresh-tasty-burger-on-wood-table.webp?b=1&s=612x612&w=0&k=20&c=EcZtAo9YEL1RWFgyPvK-1CIWq3ufeLxebmxVdUOD5gk=',
+        [
+          new Ingredient('Buns', 2),
+          new Ingredient('Meat', 1), 
+        ])
+  ];
+
+  constructor( private slService: ShoppingListService){
+
+  }
+
+  getRecipes(){
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number){
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number){
+    this.recipes.splice(index,1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+}

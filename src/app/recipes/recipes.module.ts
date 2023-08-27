79ebccdd8acbf70a6645ec81898c2f3d373a3669
Apor <peterapor04@yahoo.com>
2023-08-27 22:16:26 +0300

    325 (fixes 324)

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipeItemComponent } from "./recipes-list/recipe-item/recipe-item.component";
 import { RecipesListComponent } from "./recipes-list/recipes-list.component";
 import { RecipesComponent } from "./recipes.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -15,6 +19,11 @@ import { RecipesComponent } from "./recipes.component";
     RecipeStartComponent,
     RecipeEditComponent
     ],
+  imports: [
+    RouterModule,
+    CommonModule,
+    ReactiveFormsModule
+  ],
   exports: [
     RecipesComponent,
     RecipesListComponent,

06ca364942b5762473043f4c31e8aa392a05f767
Apor <peterapor04@yahoo.com>
2023-08-27 22:11:06 +0300

    324

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipeItemComponent } from "./recipes-list/recipe-item/recipe-item.component";
+import { RecipesListComponent } from "./recipes-list/recipes-list.component";
+import { RecipesComponent } from "./recipes.component";
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipesListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent
+    ],
+  exports: [
+    RecipesComponent,
+    RecipesListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent
+  ]
+})
+
+export class RecipesModule {}

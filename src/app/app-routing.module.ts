64318aad56074fd6b22af274f3d176dd4fe9a5a5
Apor <peterapor04@yahoo.com>
2023-08-28 09:20:40 +0300

    333

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 
 const appRoutes: Routes = [
-  {path: '', redirectTo:'/recipes', pathMatch:'full'}
+  {path: '', redirectTo:'/recipes', pathMatch:'full'},
+  {path: 'recipes',
+  loadChildren:  () => import('./recipes/recipes.module').then(m => m.RecipesModule)}
 ];
 
 

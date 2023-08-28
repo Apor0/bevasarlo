48d4ce28776c6b74d29e46d76a33d52126fa1cfd
Apor <peterapor04@yahoo.com>
2023-08-28 10:27:15 +0300

    336.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
-import { CoreModule } from 'src/core.module';
+import { CoreModule } from 'src/app/core.module';
 
 @NgModule({
   declarations: [
--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -4,9 +4,6 @@ import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
 import { RecipeService } from "./recipes/recipe.service";
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
-import { LoggingService } from "./logging.service";
-
-
 
 @NgModule({
     providers: [

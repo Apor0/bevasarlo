ae5cba52641f2fb63bea6b1b6b77abfba83f5922
Apor <peterapor04@yahoo.com>
2023-08-28 10:38:48 +0300

    341

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+
+export const environment = {
+    production: true,
+    firebaseAPIKey: 'AIzaSyCnQL26bYEm499MHn-J-WSHoD0gt_rBmkw'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+
+export const environment = {
+    production: false,
+    firebaseAPIKey: 'AIzaSyCnQL26bYEm499MHn-J-WSHoD0gt_rBmkw'
+};
